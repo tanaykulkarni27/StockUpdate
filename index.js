0b441b2e8708a90dbf94399432bcea0d047c9250
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2022-12-10 10:41:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('express')();
 const {home,Specific} = require('./urls');
 server.get("/",home);
 server.get('/view/:id',Specific);
-server.listen(3000);
+server.listen(1337);
 
